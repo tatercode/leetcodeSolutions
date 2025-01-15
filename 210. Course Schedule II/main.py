aa4819a7ac573c8ff63916ca6a4fd926eccc3629
Taven Tran <taventran@Tavens-MacBook-Pro.local>
2025-01-15 11:08:01 -0600

    Added two more problems

--- 210. Course Schedule II/main.py
+++ 210. Course Schedule II/main.py
@@ -9,13 +9,28 @@ class Solution:
         for crs, pre in prerequisites:
             preMap[crs].append(pre)
 
+        visitSet = set()
+        cycle = set()
         def dfs(crs):
-            print(crs)
-            pass
+            if crs in cycle:
+                return False
+            if crs in visitSet:
+                return True
+            
+            cycle.add(crs)
 
-        
-        for crs in preMap.keys():
-            dfs(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            
+            visitSet.add(crs)
+            cycle.remove(crs)
+            ans.append(crs)
+
+            return True 
+
+        for crs in range(numCourses):
+            if not dfs(crs): return []
 
         return ans
 
@@ -24,4 +39,7 @@ if __name__ == "__main__":
     sol = Solution()
     test = [[1, 0]]
     ans = sol.findOrder(2, test)
+    prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+    ans2 = sol.findOrder(4, prerequisites)
     print(ans)
+    print(ans2)
